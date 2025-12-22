93db969d3c46cab1071e0a7601445b632b7cb367
siam-khan-alt <nssiam99@gmail.com>
2025-12-22 08:27:09 +0600

    first commit with basic setup and collur set

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='min-h-screen'>
+      <h1 className='heading-gradient flex items-center text-center text-6xl'>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
